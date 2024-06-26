83e7f822590fcc3534289d6ec53b00358c75ec3c
kasyan1337 <tristokorun@gmail.com>
2024-06-26 10:02:16 +0800

    Course + Lesson urls.py

---
+++ materials/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet, LessonDetailView, LessonListCreateAPIView
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('lessons/', LessonListCreateAPIView.as_view(), name='lesson-list-create'),
+    path('lessons/<int:pk>/', LessonDetailView.as_view(), name='lesson-detail'),
+
+]
