7bfe2da8f5ed267bd570b0377f868d52d7cb5d5c
kasyan1337 <tristokorun@gmail.com>
2024-07-02 21:19:07 +0800

    lesson information in course serializer  - serializers.py + paths to courses in urls.py

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,19 +2,20 @@ from rest_framework import serializers
 
 from materials.models import Course, Lesson
 
-
-class CourseSerializer(serializers.ModelSerializer):
-    lesson_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = ['id', 'title', 'preview', 'description', 'lesson_count']
-
-    def get_lesson_count(self, obj):
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'preview', 'description', 'lesson_count', 'lessons']
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
